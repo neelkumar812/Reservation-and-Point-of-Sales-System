3232109c9df18788b20c6250a0997e7eb0a7d980
yz-koh <yzworkk@gmail.com>
2021-11-05 23:06:15 +0800

    Create maven quickstart

---
+++ src/main/java/com/cz2002/ss10/App.java
@@ -0,0 +1,13 @@
+package com.cz2002.ss10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
