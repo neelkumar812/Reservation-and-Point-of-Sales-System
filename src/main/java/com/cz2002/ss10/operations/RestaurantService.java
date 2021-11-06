94ce68ccbef287d93041049f927650fdf6afeda2
yz-koh <yzworkk@gmail.com>
2021-11-06 11:54:14 +0800

    Revert checkReservation to non-static and create custom ReservationNotFound exception

--- src/main/java/com/cz2002/ss10/operations/RestaurantService.java
+++ src/main/java/com/cz2002/ss10/operations/RestaurantService.java
@@ -9,63 +9,79 @@ import java.time.*;
 
 public class RestaurantService {
 
-	public ArrayList<Table> getAvailableTables() {
-		// TODO - implement RestaurantService.getAvailableTables
-		throw new UnsupportedOperationException();
-	}
+    public RestaurantService(){}
 
-	/**
-	 * 
-	 * @param tableNumber
-	 * @param orderId
-	 */
-	public void settlePayment(int tableNumber, int orderId) {
-		// TODO - implement RestaurantService.settlePayment
-		throw new UnsupportedOperationException();
-	}
+    public ArrayList<Table> getAvailableTables() {
+        // TODO - implement RestaurantService.getAvailableTables
+        throw new UnsupportedOperationException();
+    }
 
-	/**
-	 * 
-	 * @param staff
-	 * @param membershipType
-	 * @param tableNumber
-	 * @param orderItems
-	 */
-	public void createNewOrder(Staff staff, String membershipType, int tableNumber, ArrayList<MenuItem> orderItems) {
-		// TODO - implement RestaurantService.createNewOrder
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * 
+     * @param tableNumber
+     * @param orderId
+     */
+    public void settlePayment(int tableNumber, int orderId) {
+        // TODO - implement RestaurantService.settlePayment
+        throw new UnsupportedOperationException();
+    }
 
-	/**
-	 * 
-	 * @param customerName
-	 * @param reservationId
-	 */
-	public void cancelReservation(String customerName, int reservationId) {
-		// TODO - implement RestaurantService.cancelReservation
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * 
+     * @param staff
+     * @param membershipType
+     * @param tableNumber
+     * @param orderItems
+     */
+    public void createNewOrder(Staff staff, String membershipType, int tableNumber, ArrayList<MenuItem> orderItems) {
+        // TODO - implement RestaurantService.createNewOrder
+        throw new UnsupportedOperationException();
+    }
 
-	/**
-	 * 
-	 * @param customerName
-	 * @param customerContact
-	 * @param dinerSize
-	 * @param reservationDate
-	 * @param reservationTime
-	 */
-	public void createNewReservation(String customerName, int customerContact, int dinerSize, LocalDate reservationDate, LocalTime reservationTime) {
-		// TODO - implement RestaurantService.createNewReservation
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * 
+     * @param customerName
+     * @param reservationId
+     */
+    public void cancelReservation(String customerName, int reservationId) {
+        // TODO - implement RestaurantService.cancelReservation
+        throw new UnsupportedOperationException();
+    }
 
-	/**
-	 * 
-	 * @param reservationId
-	 */
-	public static void checkReservation(int reservationId) {
-		// TODO - implement RestaurantService.checkReservation
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * 
+     * @param customerName
+     * @param customerContact
+     * @param dinerSize
+     * @param reservationDate
+     * @param reservationTime
+     */
+    public void createNewReservation(String customerName, int customerContact, int dinerSize, LocalDate reservationDate,
+            LocalTime reservationTime) {
+        // TODO - implement RestaurantService.createNewReservation
+        throw new UnsupportedOperationException();
+    }
+
+    public class ReservationNotFoundException extends Exception {
+        public ReservationNotFoundException() {
+        }
+
+        public ReservationNotFoundException(String message) {
+            super(message);
+        }
+    }
+
+    /**
+     * 
+     * @param reservationId
+     */
+    public void checkReservation(int reservationId) {
+        // TODO - implement RestaurantService.checkReservation
+        try {
+            throw new ReservationNotFoundException("Reservation of id: " + reservationId + " not found!");
+        } catch (ReservationNotFoundException err) {
+            // handle error
+        }
+    }
 
 }
