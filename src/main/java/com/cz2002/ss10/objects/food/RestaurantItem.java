31838574df9d37ec377b85cc1a93f29d3d9fa60e
snesboola <63914230+snesboola@users.noreply.github.com>
2021-11-07 16:20:18 +0800

    Prelim Accessors and Mutators

--- src/main/java/com/cz2002/ss10/objects/food/RestaurantItem.java
+++ src/main/java/com/cz2002/ss10/objects/food/RestaurantItem.java
@@ -1,56 +1,102 @@
 package com.cz2002.ss10.objects.food;
 
 public class RestaurantItem {
-    private double price;
-	private String description;
-	private String name;
-    private int noOfAvailability;
-
-    RestaurantItem(){
-    }
-
-    RestaurantItem(double price, String description, String name, int noOfAvailability){
-        this.price = price;
-        this.description = description;
-        this.name = name;
-        this.noOfAvailability = noOfAvailability;
-    }
-
-
-	public double getPrice() {
-		return this.price;
-	}
-
-	/**
-	 * 
-	 * @param price
-	 */
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public String getDescription() {
-		return this.description;
-	}
-
-	/**
-	 * 
-	 * @param description
-	 */
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	/**
-	 * 
-	 * @param name
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
     
+    /**
+     * The ID of the restaurant item.
+     */
+    private int restaurantItemID;
+    /**
+     * The name of the restaurant item.
+     */
+    private String restaurantItemName;
+    /**
+     * The price of the restaurant item.
+     */
+    private double restaurantItemPrice;
+
+    /**
+     * Constructor to pass in all required parameters for a restaurant item.
+     *
+     * @param restaurantItemID    This restaurant item's ID.
+     * @param restaurantItemName  This restaurant item's name.
+     * @param restaurantItemPrice This restaurant item's price.
+     */
+    RestaurantItem(int restaurantItemID, String restaurantItemName, double restaurantItemPrice) {
+        this.restaurantItemID = restaurantItemID;
+        this.restaurantItemName = restaurantItemName;
+        this.restaurantItemPrice = restaurantItemPrice;
+    }
+
+     /**
+     * Accessor for Restaurant Item type.
+     *
+     * @return Gets the restaurant item's ID.
+     */
+    public int getId() {
+        return restaurantItemID;
+    }
+
+    /**
+     * Mutator for Restaurant Item ID.
+     *
+     * @param restaurantItemID Sets the restaurant item's ID.
+     */
+    public void setId(int restaurantItemID) {
+        this.restaurantItemID = restaurantItemID;
+    }
+
+    /**
+     * Accessor for Restaurant Item name.
+     *
+     * @return Gets the restaurant item's name.
+     */
+    public String getName() {
+        return restaurantItemName;
+    }
+
+    /**
+     * Mutator for Restaurant Item name.
+     *
+     * @param restaurantItemName Sets the restaurant item's name.
+     */
+    public void setName(String restaurantItemName) {
+        this.restaurantItemName = restaurantItemName;
+    }
+
+    /**
+     * Accessor for Restaurant Item price.
+     *
+     * @return Gets the restaurant item's price.
+     */
+    public double getPrice() {
+        return restaurantItemPrice;
+    }
+
+    /**
+     * Mutator for Restaurant Item price.
+     *
+     * @param restaurantItemPrice Sets the restaurant item's price.
+     */
+    public void setPrice(double restaurantItemPrice) {
+        this.restaurantItemPrice = restaurantItemPrice;
+    }
+
+    /**
+     * A method to read from a CSV string to convert to an object instance.
+     * This needs to be overridden if CSV data must be retrieved from a file.
+     *
+     * @param csv A string array of the CSV file.
+     */
+// To DO
+
+    /**
+     * A method to convert to CSV.
+     * This needs to be overridden if files need to be saved to CSV.
+     *
+     * @return A String array of the CSV file.
+     */
+//To Do
+
+
 }
