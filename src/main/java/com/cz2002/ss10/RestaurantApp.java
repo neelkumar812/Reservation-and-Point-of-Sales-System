6ac56964522063a8989491033d374e9f462b94e1
neelkumar812 <neelkumar812@gmail.com>
2021-11-07 20:45:25 +0800

    Not fully polished

--- src/main/java/com/cz2002/ss10/RestaurantApp.java
+++ src/main/java/com/cz2002/ss10/RestaurantApp.java
@@ -20,6 +20,7 @@ public class RestaurantApp {
 	private LocalTime openingTime;
 	private LocalTime closingTime;
 	private ArrayList<Order> orders;
+	Scanner sc = new Scanner(System.in);
 
 	public void setPromotionItems(ArrayList<PromoItem> promotionItems) {
 		this.promotionItems = promotionItems;
@@ -48,7 +49,42 @@ public class RestaurantApp {
 	 * @param operation
 	 */
 	public void handleService(int operation) {
-		// TODO - implement RestaurantApp.handleService
+		String isMember;
+        int flag = 0;
+        
+
+
+
+        int discount; //discount rate for members is 10%
+        System.out.println("Welcome to the order creation page!");
+        do{
+            System.out.println("Is the customer a member? ");
+            isMember = sc.next().toLowerCase();
+            if(isMember == "yes"){
+                flag = 0;
+            }
+            else if(isMember == "no"){
+                flag = 0;
+            }
+            else{
+                flag = 1;
+                System.out.println("Please enter either 'yes' or 'no' only");
+            }
+        }while(flag != 1);
+        do{
+            System.out.println("Please enter table number");
+            isMember = sc.next().toLowerCase();
+            if(isMember == "yes"){
+                flag = 0;
+            }
+            else if(isMember == "no"){
+                flag = 0;
+            }
+            else{
+                flag = 1;
+                System.out.println("Please enter either 'yes' or 'no' only");
+            }
+        }while(flag != 1);
 		throw new UnsupportedOperationException();
 	}
 
--- src/main/java/com/cz2002/ss10/operations/RestaurantService.java
+++ src/main/java/com/cz2002/ss10/operations/RestaurantService.java
@@ -3,24 +3,41 @@ package com.cz2002.ss10.operations;
 import com.cz2002.ss10.objects.logistics.*;
 import com.cz2002.ss10.objects.person.*;
 import com.cz2002.ss10.objects.food.*;
-
 import java.util.*;
 import java.time.*;
 
 public class RestaurantService {
 
-    public RestaurantService(){}
 
+    Scanner sc = new Scanner(System.in);
+    private static final int SEATCAP = 25;
+    private Table[] tableList = new Table[SEATCAP];
+
+    //Constructor to initialize all tables in increasing order of seating capacity.
+    public RestaurantService(){
+        for(int i = 0;i<SEATCAP; i++){
+            if(i<5) tableList[i] = new Table(2, i+1, false);
+            else if(i<10 && i>4) tableList[i] = new Table(4, i+1, false);
+            else if(i<15 && i>9) tableList[i] = new Table(6, i+1, false);
+            else if(i<20 && i>14) tableList[i] = new Table(8, i+1, false);
+            else if(i>19) tableList[i] = new Table(10, i+1, false);
+        }
+    }
     public ArrayList<Table> getAvailableTables() {
-        // TODO - implement RestaurantService.getAvailableTables
+
+        ArrayList<Table> temp = new ArrayList<Table>();
+        for(int i = 0; i<SEATCAP;i++){
+            if(tableList[i].getOccupancy() == false) temp.add(tableList[i]);
+        }
+        return temp;
         throw new UnsupportedOperationException();
     }
-
     /**
      * 
      * @param tableNumber
      * @param orderId
      */
+
     public void settlePayment(int tableNumber, int orderId) {
         // TODO - implement RestaurantService.settlePayment
         throw new UnsupportedOperationException();
@@ -34,7 +51,11 @@ public class RestaurantService {
      * @param orderItems
      */
     public void createNewOrder(Staff staff, String membershipType, int tableNumber, ArrayList<MenuItem> orderItems) {
-        // TODO - implement RestaurantService.createNewOrder
+        
+
+
+
+
         throw new UnsupportedOperationException();
     }
 
