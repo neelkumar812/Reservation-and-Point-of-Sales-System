641940956046094b1719a18013db0c95bf798a69
snesboola <63914230+snesboola@users.noreply.github.com>
2021-11-07 16:18:43 +0800

    Prelim Accessors and Mutators

--- src/main/java/com/cz2002/ss10/objects/food/PromoItem.java
+++ src/main/java/com/cz2002/ss10/objects/food/PromoItem.java
@@ -2,38 +2,122 @@ package com.cz2002.ss10.objects.food;
 
 import java.util.*;
 
-public class PromoItem extends RestaurantItem {
+/**
+ * PromoItem Class
+ *
+ * @author Sneha Ravisankar
+ * @version 1.0
+ * @since 2021-11-06
+ */
 
-	private ArrayList<MenuItem> promotionItems;
+ public class PromoItem extends RestaurantItem{
 
-	public ArrayList<MenuItem> getPromotionItems() {
-		return this.promotionItems;
-	}
+	private ArrayList<MenuItem> PromoItems;
+	/**
+     * The ID of the Promo item's components.
+     */
+    private int promoMain;
+    private int promoDessert;
+    private int promoDrink;
 
-	PromoItem(){
-		super();
-	}
+	/**
+     * Constructor to pass in all attributes
+     *
+     * @param promoID      This Promo item's ID.
+     * @param promoName    This Promo item's name.
+     * @param promoPrice   This Promo item's price.
+     * @param promoMain    This Promo item's main ID.
+     * @param promoDessert This Promo item's dessert ID.
+     * @param promoDrink   This Promo item's drink ID.
+     */
+    public PromoItem(int promoID, String promoName, double promoPrice, int promoMain, int promoDessert, int promoDrink) {
+        
+		super(promoID, promoName, promoPrice); //called from parent
+        this.promoMain = promoMain;
+        this.promoDessert = promoDessert;
+        this.promoDrink = promoDrink;
+    }
 
-	PromoItem(double price, String description, String name, int noOfAvailability, ArrayList<MenuItem> promotionItems){
-		super(price, description, name, noOfAvailability);
-		this.promotionItems = promotionItems;
+	public ArrayList<MenuItem> getPromoItems() {
+
+		//Is this how an Array List Returns? 
+		return PromoItems;
 	}
 
 	/**
 	 * 
-	 * @param promotionItem
+	 * @param PromoItem
 	 */
-	public void addPromotionItem(MenuItem promotionItem) {
-		// TODO - implement PromotionSet.addPromotionItem
+	public void addPromoItem(MenuItem PromoItem) {
+		// TODO - implement PromoSet.addPromoItem
+
+		// How to add items to an Array List
+
+
 		throw new UnsupportedOperationException();
 	}
 
 	/**
 	 * 
-	 * @param promotionItems
+	 * @param PromoItems
 	 */
-	public void setPromotionItems(ArrayList<MenuItem> promotionItems) {
-		this.promotionItems = promotionItems;
+	public void setPromoItems(ArrayList<MenuItem> PromoItems) {
+		this.PromoItems = PromoItems;
 	}
+
+	/**
+     * Accessor for Promotion item's main.
+     *
+     * @return Gets the Promotion item's main.
+     */
+    public int getPromoMain() {
+        return promoMain;
+    }
+
+    /**
+     * Mutator for Promotion item's main.
+     *
+     * @param promoMain Sets the Promotion item's main.
+     */
+    public void setPromoMain(int promoMain) {
+        this.promoMain = promoMain;
+    }
+
+    /**
+     * Accessor for Promotion item's dessert.
+     *
+     * @return Gets the Promotion item's dessert.
+     */
+    public int getPromoDessert() {
+        return promoDessert;
+    }
+
+    /**
+     * Mutator for Promotion item's main.
+     *
+     * @param promoDessert Sets the Promotion item's dessert.
+     */
+    public void setPromoDessert(int promoDessert) {
+        this.promoDessert = promoDessert;
+    }
+
+    /**
+     * Accessor for Promotion item's drink.
+     *
+     * @return Gets the Promotion item's drink.
+     */
+    public int getPromoDrink() {
+        return promoDrink;
+    }
+
+    /**
+     * Mutator for Promotion item's drink.
+     *
+     * @param promoDrink Sets the Promotion item's drink.
+     */
+    public void setPromoDrink(int promoDrink) {
+        this.promoDrink = promoDrink;
+    }
+
 
 }
