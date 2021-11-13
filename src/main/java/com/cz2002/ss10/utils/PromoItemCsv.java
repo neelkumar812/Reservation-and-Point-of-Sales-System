031589bc35ea26906fc2373675ca8bfe3f038e09
yz-koh <yzworkk@gmail.com>
2021-11-14 00:57:32 +0800

    Add insert promo item list from csv

--- src/main/java/com/cz2002/ss10/utils/PromoItemCsv.java
+++ src/main/java/com/cz2002/ss10/utils/PromoItemCsv.java
@@ -1,5 +1,6 @@
 package com.cz2002.ss10.utils;
 
+import com.cz2002.ss10.RestaurantApp;
 import com.cz2002.ss10.objects.food.*;
 
 import com.opencsv.*;
@@ -12,6 +13,9 @@ import java.io.FileReader;
 
 public class PromoItemCsv implements IExtractCsv {
 
+	/**
+	 * @param filePath path on which csv lies on
+	 */
     public Map<String, List<String>> extractInputFromCSV(String filePath) {
 
 		Map<String, List<String>> promoItemMap = new HashMap<String, List<String>>();
@@ -20,7 +24,7 @@ public class PromoItemCsv implements IExtractCsv {
 			String[] promoItems = r.stream().toArray(String[]::new);
 			int index = 0;
 			while (promoItems.length > index) {
-				ArrayList<String> promoInfo = new ArrayList<String>();
+				List<String> promoInfo = new ArrayList<String>();
 				promoInfo.addAll(Arrays.asList(promoItems[1], promoItems[2], promoItems[3], promoItems[4], promoItems[5], promoItems[6], promoItems[7]));
 				promoItemMap.put(promoItems[0],promoInfo);
 			}
@@ -44,6 +48,13 @@ public class PromoItemCsv implements IExtractCsv {
 	 * @param promoList
 	 */
 	public void insertPromoList(Map<String, List<String>> promoList) {
+		for (Map.Entry<String, List<String>> entry : promoList.entrySet()) {
+			// get promo info
+			List<String> promoInfo = entry.getValue();
+
+			// add PromoItem object to PromoItems ArrayList in RestaurantApp
+			RestaurantApp.promotionItems.add(new PromoItem((int)Integer.parseInt(entry.getKey()), promoInfo.get(0), Double.valueOf(promoInfo.get(1)), (int)Integer.valueOf(promoInfo.get(2)), (int)Integer.valueOf(promoInfo.get(3)), (int)Integer.valueOf(promoInfo.get(4)), (int)Integer.valueOf(promoInfo.get(5))));
+		}
 		
 	}
 
