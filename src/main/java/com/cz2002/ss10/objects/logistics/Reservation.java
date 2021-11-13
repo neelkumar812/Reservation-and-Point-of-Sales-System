8c1cc3aa3f321b9171cf3ec6988e5798c83415f9
GIDEON <gmanik30>
2021-11-13 13:30:42 +0800

    add cancelreservation method

--- src/main/java/com/cz2002/ss10/objects/logistics/Reservation.java
+++ src/main/java/com/cz2002/ss10/objects/logistics/Reservation.java
@@ -4,14 +4,14 @@ import java.time.*;
 
 public class Reservation {
 
+	private int reservationId;
 	private LocalDate reservationDate;
 	private LocalTime reservationTime;
-	private int tableNumber;
-	private int dinerSize;
-	private String customerName;
 	private int customerContact;
-	private int reservationId;
-
+	private int tableNumber;
+	private String customerName;
+	private int dinerSize;
+	
 	/**
 	 * 
 	 * @param resId
@@ -35,13 +35,29 @@ public class Reservation {
         return this.reservationId;
     }
 
-    public int getTableNumber(){
+	public LocalDate getReservationDate(){
+        return this.reservationDate;
+    }
+
+    public LocalDate getReservationTime(){
+        return this.reservationTime;
+    }
+
+	public int getCustomerContact(){
+        return this.customerContact;
+    }
+
+	public int getTableNumber(){
         return this.tableNumber;
+    }
+
+	public int getDinerSize(){
+        return this.dinerSize;
     }
 
 	public void cancelReservation() {
 		// TODO - implement Reservation.cancelReservation
-		this = null;
+		System.out.println("Reservation for "+ this.customerName + "is cancelled (Reservation ID: "+ this.reservationId+")");
 	}
 
 }
