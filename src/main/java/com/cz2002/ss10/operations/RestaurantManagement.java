a30ee00cee6f7c552a96c063a3775735434a29a3
yz-koh <yzworkk@gmail.com>
2021-11-14 12:39:18 +0800

    Update restaurant management operations

--- src/main/java/com/cz2002/ss10/operations/RestaurantManagement.java
+++ src/main/java/com/cz2002/ss10/operations/RestaurantManagement.java
@@ -1,40 +1,76 @@
 package com.cz2002.ss10.operations;
 
-import com.cz2002.ss10.objects.person.*;
-import com.cz2002.ss10.objects.logistics.*;
+import com.cz2002.ss10.RestaurantApp;
 
-import java.util.ArrayList;
+import com.cz2002.ss10.objects.person.*;
+import com.cz2002.ss10.utils.RevenueReportCsv;
+import com.cz2002.ss10.utils.StaffCsv;
+import com.cz2002.ss10.objects.logistics.*;
+import com.cz2002.ss10.utils.RevenueReportCsv.ReportDuration;
+
+import java.io.IOException;
+import java.lang.Exception;
+import java.util.*;
 
 public class RestaurantManagement {
 
 	/**
+	 * Manually insertion of new staff
 	 * 
 	 * @param name
 	 * @param gender
 	 * @param employeeID
 	 * @param jobTitle
 	 */
-	public Staff addStaff(String name, char gender, int employeeID, String jobTitle) {
-		// TODO - implement RestaurantManagement.addStaff
-		throw new UnsupportedOperationException();
+	public void addStaff(String name, char gender, int employeeID, String jobTitle) {
+		RestaurantApp.staffs.add(new Staff(name, gender, employeeID, jobTitle));
+	}
+
+	/**
+	 * Mass insertion of new staff
+	 * 
+	 * @param filePath
+	 */
+	public void addStaff(String filePath) {
+		try {
+			StaffCsv staffCsvHelper = new StaffCsv();
+			Map<String, List<String>> staffMap = staffCsvHelper.extractInputFromCSV(filePath);
+			staffCsvHelper.insertStaffList(staffMap);
+		} catch (Exception error) {
+			System.out.println("Failed to insert new staff from CSV! " + error.toString());
+		}
 	}
 
 	/**
 	 * 
 	 * @param orders
 	 */
-	public void generateRevenueReport(ArrayList<Order> orders) {
-		// TODO - implement RestaurantManagement.generateRevenueReport
-		throw new UnsupportedOperationException();
+	public void generateRevenueReport(ArrayList<Order> orders, ReportDuration reportPeriod, String filePath) {
+		try {
+			RevenueReportCsv revReportHelper = new RevenueReportCsv();
+			revReportHelper.exportOutputToCSV(revReportHelper.formatReportToString(orders, reportPeriod), filePath);
+		} catch (IOException error) {
+			System.out.println("Failed I/O operation when exporting to CSV!\n " + error.toString());
+		}
 	}
 
 	/**
+	 * Iterate through staff list and return true if removed
 	 * 
 	 * @param employeeID
 	 */
-	public Staff removeStaff(int employeeID) {
-		// TODO - implement RestaurantManagement.removeStaff
-		throw new UnsupportedOperationException();
+	public boolean removeStaff(int employeeID) {
+		// find index in staff list
+
+		// remove staff by index
+		Iterator<Staff> iter = RestaurantApp.staffs.iterator();
+		while (iter.hasNext()) {
+			if (((Integer) iter.next().getStaffID()).equals(employeeID)) {
+				iter.remove();
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
