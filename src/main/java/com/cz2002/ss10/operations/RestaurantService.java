a7c29a15a0ea5c5cf8bcdb051009e1b623bd7741
yz-koh <yzworkk@gmail.com>
2021-11-15 16:09:05 +0800

    Add invoice item printing

--- src/main/java/com/cz2002/ss10/operations/RestaurantService.java
+++ src/main/java/com/cz2002/ss10/operations/RestaurantService.java
@@ -7,15 +7,16 @@ import com.cz2002.ss10.objects.food.*;
 import java.util.*;
 import java.time.*;
 
-
 public class RestaurantService {
 
-    public static final int TABLESEATCAP =25;
+    public static final int TABLESEATCAP = 25;
     private static int cancellationCounter = 1000;
     Scanner sc = new Scanner(System.in);
 
-    //Constructor
-    public RestaurantService(){}
+    // Constructor
+    public RestaurantService() {
+    }
+
     /**
      * @param staff
      * @param membershipType
@@ -24,71 +25,80 @@ public class RestaurantService {
      * @param orderItems
      */
 
-    public static void createNewOrder(Staff staff, Boolean membershipType, int tableNumber, int orderID, ArrayList<RestaurantItem> orderItems)
-     {
+    public static void createNewOrder(Staff staff, Boolean membershipType, int tableNumber, int orderID,
+            ArrayList<RestaurantItem> orderItems) {
         Order temp = new Order(staff, membershipType, tableNumber, orderID, orderItems);
         RestaurantApp.orders.add(temp);
         System.out.println("New order created!");
-        
+
     }
 
-
-
-    //SettlePayments essentially does all the calculation stuff, and calls PrintOrderInvoice in the end
-    public static void settlePayment(int tableNumber, int orderId) 
-    {
+    // SettlePayments essentially does all the calculation stuff, and calls
+    // PrintOrderInvoice in the end
+    public static void settlePayment(int tableNumber, int orderId) {
         double current = 0;
         double temp;
         double serviceChrg = 0;
         double gst = 0;
         double curTotal;
         double finalTotal = 0;
+        int orderIndex = 0;
 
-        for(int i = 0; i<RestaurantApp.orders.size();i++)
-        {
-            if(RestaurantApp.orders.get(i).getOrderId() == orderId)
-            {
-                RestaurantApp.orders.get(i).markAsPaid(); 
-                RestaurantApp.tables.get(tableNumber-1).setOccupancy(false);
+        for (int i = 0; i < RestaurantApp.orders.size(); i++) {
+            if (RestaurantApp.orders.get(i).getOrderId() == orderId) {
+                orderIndex = i;
+                RestaurantApp.orders.get(i).markAsPaid();
+                RestaurantApp.tables.get(tableNumber - 1).setOccupancy(false);
                 System.out.println("Order paid and table is now available");
-            }
-            else System.out.println("OrderID not found");
+            } else
+                System.out.println("OrderID not found");
         }
-        //Calculating Total for This Order
-        for(int i = 0; i<RestaurantApp.orders.size();i++)
-        {
-            if(RestaurantApp.orders.get(i).getOrderId() == orderId) current = RestaurantApp.orders.get(i).getSubtotal();
-            if(RestaurantApp.orders.get(i).getMembershipType() == true)
-            {
+        // Calculating Total for This Order
+        for (int i = 0; i < RestaurantApp.orders.size(); i++) {
+            if (RestaurantApp.orders.get(i).getOrderId() == orderId)
+                current = RestaurantApp.orders.get(i).getSubtotal();
+            if (RestaurantApp.orders.get(i).getMembershipType() == true) {
                 temp = 0.1 * current;
                 current = current - temp;
-            } 
-            serviceChrg = 0.1*current;
+            }
+            serviceChrg = 0.1 * current;
             curTotal = serviceChrg + current;
-            gst = 0.07*curTotal;
+            gst = 0.07 * curTotal;
             finalTotal = gst + curTotal;
-            
+
         }
-        printOrderInvoice(current, serviceChrg, gst, finalTotal);
-        
+        printOrderInvoice(current, serviceChrg, gst, finalTotal, RestaurantApp.orders.get(orderIndex));
+
     }
 
-    //Just prints the orderInvoice in a receipt format
-    public static void printOrderInvoice(double subtotal, double serviceCharge, double GST, double finalTotal) {
-		
+    // Just prints the orderInvoice in a receipt format
+    public static void printOrderInvoice(double subtotal, double serviceCharge, double GST, double finalTotal,
+            Order order) {
+
         System.out.println("********************************************************************");
         System.out.println("                Thank you for dining with us today!");
         System.out.println("Date:" + LocalDate.now() + "                 Time:" + LocalTime.now());
         System.out.println("--------------------------------------------------------------------");
-        System.out.println("Quanity | Item                                          | Price    |");
-
-        //TODO-
-
-
-
-
-        //TODO-
-
+        System.out.println("Quantity | Item                                          | Price    |");
+        Iterator<RestaurantItem> orderIterator = order.getOrderItems().iterator();
+        HashMap<String, Integer> itemHashMap = new HashMap<String, Integer>();
+        while (orderIterator.hasNext()) {
+            // update sale count for every item sold
+            String key = orderIterator.next().getName();
+            if (itemHashMap.containsKey(key)) {
+                itemHashMap.put(key, itemHashMap.get(key) + 1);
+            } else {
+                itemHashMap.put(key, 1);
+            }
+        }
+        // HashMap<orderItemName, [quantity, price*quantity]>
+        HashMap<String, ArrayList<String>> finalItemHashMap = new HashMap<String, ArrayList<String>>();
+        for (String key : itemHashMap.keySet()) {
+            finalItemHashMap.put(key, addNameQuantityAndPrice(key, itemHashMap.get(key).toString(), order.getOrderItems()));
+        }
+        for (ArrayList<String> value : finalItemHashMap.values()) {
+            System.out.println(value.get(1) + "  " + value.get(0) + "           " + value.get(2));
+        }
         System.out.println("********************************************************************");
         System.out.println("Subtotal:                                          $" + subtotal);
         System.out.println("-----------------------------------------------------------");
@@ -97,8 +107,29 @@ public class RestaurantService {
         System.out.println("-----------------------------------------------------------");
         System.out.println("Final Total:                                       $" + finalTotal);
         System.out.println("                Payment made via credit/debit card :) .             ");
-        
+
     }
+
+    private static ArrayList<String> addNameQuantityAndPrice(String itemName, String itemQuantity, ArrayList<RestaurantItem> orderedItems) {
+		ArrayList<String> itemInfo = new ArrayList<String>();
+
+		// get item price
+		String itemPrice = "0.00";
+		int index = 0;
+		while (orderedItems.size() > index){
+				if (orderedItems.get(index).getName() == itemName) {
+					itemPrice = String.valueOf(orderedItems.get(index).getPrice()* Double.valueOf(itemQuantity));
+				}
+				index ++;
+		}
+
+
+		// update quantity and price of ordered item
+		itemInfo.addAll(Arrays.asList(itemName,itemQuantity, itemPrice));
+		return itemInfo;
+
+	}
+
     /**
      * 
      * @param customerName
@@ -107,7 +138,8 @@ public class RestaurantService {
      * @param reservationDate
      * @param reservationTime
      */
-    public static void createNewReservation(String customerName, int customerContact, int dinerSize, int tableNumber, int reservationID) {
+    public static void createNewReservation(String customerName, int customerContact, int dinerSize, int tableNumber,
+            int reservationID) {
         Reservation temp = new Reservation(customerName, customerContact, dinerSize, tableNumber, reservationID);
         RestaurantApp.reservations.add(temp);
         System.out.println("Reservation ssuccessfully created");
@@ -119,19 +151,18 @@ public class RestaurantService {
      * @param reservationId
      */
     public static void cancelReservation(String customerName, int reservationId) {
-        for(int i = 0; i<RestaurantApp.reservations.size();i++){
-            if(RestaurantApp.reservations.get(i).getReservationId() == reservationId)
-            {
+        for (int i = 0; i < RestaurantApp.reservations.size(); i++) {
+            if (RestaurantApp.reservations.get(i).getReservationId() == reservationId) {
                 RestaurantApp.reservations.remove(i);
                 System.out.println("Reservation for " + customerName + "has been cancelled");
                 cancellationCounter++;
-                System.out.println("Cancellation ID: " + cancellationCounter );
+                System.out.println("Cancellation ID: " + cancellationCounter);
                 break;
             }
-            
+
         }
         System.out.println("Error cancelling reservation, please try again!");
-        
+
     }
 
     /**
@@ -141,13 +172,16 @@ public class RestaurantService {
     public static void checkReservation(int reservationID) {
 
         boolean existResv = false;
-        for(int i = 0;i<=RestaurantApp.reservations.size();i++){
-            if(RestaurantApp.reservations.get(i).getReservationId() == reservationID){
-                System.out.println("There is a reservation for "+ RestaurantApp.reservations.get(i).getDinerSize() + " people who are designated table number " + RestaurantApp.reservations.get(i).getTableNumber());
+        for (int i = 0; i <= RestaurantApp.reservations.size(); i++) {
+            if (RestaurantApp.reservations.get(i).getReservationId() == reservationID) {
+                System.out.println("There is a reservation for " + RestaurantApp.reservations.get(i).getDinerSize()
+                        + " people who are designated table number "
+                        + RestaurantApp.reservations.get(i).getTableNumber());
                 System.out.println("Reservation details are as follows: ");
                 System.out.println("---------------------");
                 System.out.println("Customer Name: " + RestaurantApp.reservations.get(i).getCustomerName());
-                System.out.println("Customer Contact Number: " + RestaurantApp.reservations.get(i).getCustomerContact());
+                System.out
+                        .println("Customer Contact Number: " + RestaurantApp.reservations.get(i).getCustomerContact());
                 System.out.println("Reservation Date: " + RestaurantApp.reservations.get(i).getReservationDate());
                 System.out.println("Reservation Time: " + RestaurantApp.reservations.get(i).getReservationTime());
                 System.out.println("Reservation ID: " + RestaurantApp.reservations.get(i).getReservationId());
@@ -155,20 +189,8 @@ public class RestaurantService {
                 break;
             }
         }
-        if(existResv == false) System.out.println("Reservation not found!");
+        if (existResv == false)
+            System.out.println("Reservation not found!");
     }
-    
-
-    
-
-
-
-
-
-
-
-
-
-    
 
 }
